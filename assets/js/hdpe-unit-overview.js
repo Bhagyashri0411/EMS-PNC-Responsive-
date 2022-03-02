93237e86188bd505c9b8012ca8d549b2ae02fe27
spatil95 <shubhampatil7629@gmail.com>
2022-03-02 20:12:39 +0530

    done

--- assets/js/hdpe-unit-overview.js
+++ assets/js/hdpe-unit-overview.js
@@ -1,19 +1,14 @@
 $(document).ready(function () {
     totalThroughput();
-    // lastupdatedTime();
     $("#bs-example-navbar-collapse-1").load("./../../templates/nav/nav.html", function () {
-        // alert('navigation loaded');
     });
     $("#left-sidebar").load("./../../templates/left-sidebar/left-sidebar.html");
     $("#hdpe-horizontal-tiles").load("./../../templates/hdpe-unit-overview/hdpe-horizontal-tiles/hdpe-horizontal-tiles.html");
-    // $("#layoutSidenav_content").load("./../../templates/dashboard/dashboard.html");
-
     $("#overall-sec-overview").load("./../../templates/hdpe-unit-overview/overall-sec-overview/overall-sec-overview.html", function () { });
     $('#hdpe-unit-overview-tab a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        var target = $(e.target).attr("href") // activated tab
+        var target = $(e.target).attr("href")
         var fileName = target.substring(1);
         $(target).load("./../../templates/hdpe-unit-overview/" + fileName + "/" + fileName + ".html", function () { });
-
     });
     const b = new Date(sessionStorage.getItem("lastUpdateddate"));
     console.log(b, 'b');
@@ -21,31 +16,14 @@ $(document).ready(function () {
     const setdate = String(b.getDate()).padStart(2, '0') + "-" + String(dmonth).padStart(2, '0') + "-" + b.getFullYear() + " " + String(b.getHours()).padStart(2, '0') + ":" + String(b.getMinutes()).padStart(2, '0') + ":" + String(b.getSeconds()).padStart(2, '0');
     document.getElementById("hdpeTimeStamp").innerHTML = setdate;
 });
-
-// function lastupdatedTime(data) {
-//     var today = new Date();
-
-//     var date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear()
-
-//     var time = today.getHours() + ":" + today.getMinutes();
-
-//     var dateTime = date + ' ' + time;
-//     document.getElementById("hdpeTimeStamp").innerHTML = dateTime;
-
-// }
 function totalThroughput(data) {
     $.ajax({
-        url: 'http://192.168.1.119:8090/auth/HDPE/feedratePlantload',
+        url: 'http://192.168.1.105:8080/auth/HDPE/feedratePlantload',
         method: "GET"
     }).done(function (data) {
-
-
         document.getElementById("totalThroughput").innerHTML = data.feedrate;
         document.getElementById("Power").innerHTML = data.plantload;
-
     });
-
-
 }
 function Truncated() {
     $.ajax({
@@ -67,9 +45,6 @@ function csvdownload() {
         const headers = Object.keys(data[0]);
         csvRows.push(headers.join(','));
         console.log(data, "name");
-
-        //console.log(csvRows, "values");
-
         for (const row of data) {
             csvRows.join('\n');
             var abc = '\n' + row.kpi_name + ',' + row.value + ',' + row.uom;
