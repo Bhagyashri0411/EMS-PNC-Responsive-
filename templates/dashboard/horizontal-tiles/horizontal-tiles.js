0caece51fe2b4b9934cffa63836d2ca905d96311
Bhagyashri0411 <bhagyashripawar0411@gmail.com>
2022-03-05 03:44:36 +0530

    done

--- templates/dashboard/horizontal-tiles/horizontal-tiles.js
+++ templates/dashboard/horizontal-tiles/horizontal-tiles.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
+    var abc = 'TOE/hr';
+    getDoughnutData(abc);
     loadGaugeChartData();
     guagevaluehomeAct();
     getcardhome1();
     getcardhome2();
-    var abc = 'Gcal/hr';
-    getDoughnutData(abc);
+
+    getcardhome3();
+    var abc = 'TOE/hr';
+
 
     $(".donougt-radio").click(function () {
         console.log($("input[name=fav_language]:checked").val());
@@ -29,7 +33,7 @@ function getDoughnutData(abc) {
         method: "POST",
         data: postdata,
 
-        url: "http://localhost:8090/home/totalfuelconsumed1",
+        url: "http://localhost:8090/home/totalfuelconsumed",
     }).done(function (data) {
         loadDoughnutChart(data);
     })
@@ -83,8 +87,8 @@ function loadDoughnutChart(data) {
             startAngle: 120,
             dataPoints: [
                 // { y: data[0].solid, name: "Solid" },
-                { y: data.liquid, name: "Liquid", indexLabel:((data.liquid / data.totalfuel) * 100).toFixed(2) + "%" },
-                { y: data.gas, name: "Gas", indexLabel:((data.gas / data.totalfuel) * 100).toFixed(2) + "%" },
+                { y: data.liquid, name: "Liquid", indexLabel: "Liquid " + ((data.liquid / data.totalfuel) * 100).toFixed(2) + "%" },
+                { y: data.gas, name: "Gas", indexLabel: "Gas " + ((data.gas / data.totalfuel) * 100).toFixed(2) + "%" },
             ]
         }]
     });
@@ -234,13 +238,47 @@ function getcardhome2() {
             "Content-Type": "application/json",
             "Authorization": sessionStorage.getItem("tokenType") + " " + sessionStorage.getItem("accessToken"),
         },
-        url: 'http://localhost:8090/home/energyintensity',
+        url: 'http://localhost:8090/home/GHGemmission',
         method: "GET"
     }).done(function (data) {
 
         document.getElementById("resulthome2").innerHTML = data.currentvalue;
         document.getElementById("ref2").innerHTML = data.refvalue;
         document.getElementById("countHorizon2").innerHTML = data.kpivalue;
+        if (data.currentvalue > 0) {
+            document.getElementById("resulthome2").innerHTML = "+" + data.currentvalue;
+        }
+        else {
+            document.getElementById("resulthome2").innerHTML = data.currentvalue;
+        }
+        $(".result").each(function () {
+            var text = $(this).text();
+            if (/[+-]?\d+(\.\d+)?/.test(text)) {
+                var num = parseFloat(text);
+                if (num < 0) {
+                    $(this).addClass("red");
+                } else if (num > 0) {
+                    $(this).addClass("green");
+                }
+
+            }
+        });
+    });
+
+}
+function getcardhome3() {
+    $.ajax({
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": sessionStorage.getItem("tokenType") + " " + sessionStorage.getItem("accessToken"),
+        },
+        url: 'http://localhost:8090/home/SpecificEnergyConsumption',
+        method: "GET"
+    }).done(function (data) {
+
+        document.getElementById("resulthome3").innerHTML = data.currentvalue;
+        document.getElementById("ref3").innerHTML = data.refvalue;
+        document.getElementById("countHorizon3").innerHTML = data.kpivalue;
         if (data.currentvalue > 0) {
             document.getElementById("resulthome2").innerHTML = "+" + data.currentvalue;
         }
