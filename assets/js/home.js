3de417d0aedc7cea5a62ea3a26e672780051ccb7
mayur <msjan25@gmail.com>
2022-03-04 20:32:34 +0530

    its done

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,3 +7,4 @@ $(document).ready(function () {
   $("#layoutSidenav_nav").load("./templates/side-nav/side-nav.html");
   $("#layoutSidenav_content").load("./templates/dashboard/dashboard.html");
 
+})
